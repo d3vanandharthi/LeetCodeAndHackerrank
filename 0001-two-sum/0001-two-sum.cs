029ea6ec3b249cdbd5abb87bab9bcadbb93f2de6
d3vanandharthi <163610051+d3vanandharthi@users.noreply.github.com>
2024-11-20 07:20:18 +0530

    Time: 1 ms (98.54%), Space: 48.4 MB (15.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+     Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (dict.ContainsKey(diff))
+            {
+                return new int[] {dict[diff], i};
+            }
+            if (!dict.ContainsKey(nums[i]))
+            {
+                dict.Add(nums[i], i);
+            }
+        }
+        return new int[]{0,0};
+    }
+}
